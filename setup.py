74cfb2b62ee335046a1f12e56f6cd45afd1e3793
h.grohne@cygnusnetworks.de <h.grohne@cygnusnetworks.de@af1ba5e2-5069-2686-dedb-b34fb0d4ce42>
2011-02-17 12:02:51 +0000

    added a setup.py
    git-svn-id: https://nflogipac.googlecode.com/svn/trunk@25 af1ba5e2-5069-2686-dedb-b34fb0d4ce42

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import distutils.core
+
+distutils.core.setup(name='nflogipac',
+	version='0.1',
+	description="netfilter NFLOG based IP accounting daemon",
+	long_description="""It allows IP based accounting of traffic that is matched by iptables rules. Instead of the ULOG the more recent NFLOG target is used. This allows accounting IPv6 traffic. Furthermore this software is a hybrid of C++ and Python. The performance of C++ is used to count the traffic and the flexibility of Python is used to store the accounting information in a (MySQL) database.""",
+	author='Helmut Grohne',
+	author_email='h.grohne@cygnusnetworks.de',
+	maintainer='Torge Szczepanek',
+	maintainer_email='debian@cygnusnetworks.de',
+	license='GNU GPLv3',
+	packages=['nflogipac'],
+	scripts=['nflogipacd.py'], # TODO: should go to sbin
+	classifiers=[
+		"Development Status :: 2 - Pre-Alpha",
+		"License :: OSI Approved :: GNU General Public License (GPL)",
+		"Operating System :: POSIX :: Linux",
+		"Intended Audience :: System Administrators",
+		"Programming Language :: C",
+		"Programming Language :: C++",
+		"Programming Language :: Python :: 2",
+		"Topic :: System :: Networking",
+		]
+	)
+

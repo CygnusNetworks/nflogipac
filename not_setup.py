b32bdff449826a468b7e4c24a31193a43175af30
Dr. Torge Szczepanek <t.szczepanek@cygnusnetworks.de>
2015-05-06 11:49:39 +0200

    Fix indenting

--- not_setup.py
+++ not_setup.py
@@ -7,25 +7,25 @@
 import distutils.core
 
 distutils.core.setup(name='nflogipac',
-					 version='0.50',
-					 description="netfilter NFLOG based IP accounting daemon",
-					 long_description="""It allows IP based accounting of traffic that is matched by iptables rules. Instead of the ULOG the more recent NFLOG target is used. This allows accounting IPv6 traffic. Furthermore this software is a hybrid of C++ and Python. The performance of C++ is used to count the traffic and the flexibility of Python is used to store the accounting information in a (MySQL) database.""",
-					 author='Helmut Grohne',
-					 author_email='h.grohne@cygnusnetworks.de',
-					 maintainer='Torge Szczepanek',
-					 maintainer_email='debian@cygnusnetworks.de',
-					 license='GNU GPLv3',
-					 packages=['nflogipac'],
-					 classifiers=[
-						 "Development Status :: 5 - Production/Stable",
-						 "License :: OSI Approved :: GNU General Public License (GPL)",
-						 "Operating System :: POSIX :: Linux",
-						 "Intended Audience :: System Administrators",
-						 "Programming Language :: C",
-						 "Programming Language :: C++",
-						 "Programming Language :: Python :: 2",
-						 "Topic :: System :: Networking",
-					 ]
+	version='0.50',
+	description="netfilter NFLOG based IP accounting daemon",
+	long_description="""It allows IP based accounting of traffic that is matched by iptables rules. Instead of the ULOG the more recent NFLOG target is used. This allows accounting IPv6 traffic. Furthermore this software is a hybrid of C++ and Python. The performance of C++ is used to count the traffic and the flexibility of Python is used to store the accounting information in a (MySQL) database.""",
+	author='Helmut Grohne',
+	author_email='h.grohne@cygnusnetworks.de',
+	maintainer='Torge Szczepanek',
+	maintainer_email='debian@cygnusnetworks.de',
+	license='GNU GPLv3',
+	packages=['nflogipac'],
+	classifiers=[
+		"Development Status :: 5 - Production/Stable",
+		"License :: OSI Approved :: GNU General Public License (GPL)",
+		"Operating System :: POSIX :: Linux",
+		"Intended Audience :: System Administrators",
+		"Programming Language :: C",
+		"Programming Language :: C++",
+		"Programming Language :: Python :: 2",
+		"Topic :: System :: Networking",
+	]
 )
 
 # vim:ts=4 sw=4
